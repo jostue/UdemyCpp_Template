7766316d99c3b7f36fcc2ae0ce37734fb3e0a9e5
JoStue <jonas-st@t-online.de>
2022-03-31 19:40:12 +0200

    Chapter 1 Exercise

--- 01_Basics/1_Exercise/exercise.cc
+++ 01_Basics/1_Exercise/exercise.cc
@@ -4,4 +4,19 @@
 
 void mod_cross_sum(int I, int J)
 {
+    for (int i_idx = 0; i_idx < I; ++i_idx)
+    {
+        for (int j_idx = 0; j_idx < J; ++j_idx)
+        {
+            std::cout << "i: " << i_idx << " j: " << j_idx << " ";
+            if ((i_idx + j_idx) % 2 == 0)
+            {
+                std::cout << ":= Gerade!" << std::endl;
+            }
+            else
+            {
+                std::cout << ":= Ungerade!" << std::endl;
+            }
+        }
+    }
 }
